34e9740c2d2b5c4525bddac1e69ce05b93bf6b7c
Pascal Simon <pascaltobiassimon@gmail.com>
2021-12-03 13:14:17 +0100

    Only use leftover readings when counting 1s

--- day03/solution_b.py
+++ day03/solution_b.py
@@ -1,18 +1,16 @@
 def filter_readings(readings, position, expected):
     return [reading for reading in readings if reading[position] == expected]
 
-l = 12
-lines = 0
+
+def count_ones_in_position(readings, position):
+    return len(filter_readings(readings, position, "1"))
+
+
 readings = []
-ones = [0] * l
 
 file = open('input.txt', 'r+')
 for line in file.readlines():
-    lines += 1
     readings.append(line)
-    for i in range(l):
-        if line[i] == '1':
-            ones[i] += 1
 file.close()
 
 o2_generator = list(readings)
@@ -20,12 +18,14 @@ co2_scrubber = list(readings)
 
 pos = 0
 while len(o2_generator) > 1:
-    o2_generator = filter_readings(o2_generator, pos, "1" if ones[pos] >= lines / 2 else "0")
+    o2_generator = filter_readings(o2_generator, pos, "1" if count_ones_in_position(
+        o2_generator, pos) >= len(readings) / 2 else "0")
     pos += 1
 
 pos = 0
 while len(co2_scrubber) > 1:
-    co2_scrubber = filter_readings(co2_scrubber, pos, "0" if ones[pos] >= lines / 2 else "1")
+    co2_scrubber = filter_readings(co2_scrubber, pos, "0" if count_ones_in_position(
+        co2_scrubber, pos) >= len(readings) / 2 else "1")
     pos += 1
 
 print("O2 Generator: ", int(o2_generator[0], 2))
