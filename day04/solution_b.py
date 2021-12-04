7e23c0aec28287d0578f10b7d31aa1074d18b13e
Pascal Simon <pascaltobiassimon@gmail.com>
2021-12-04 23:36:25 +0100

    Add solution for day 4, part 2

---
+++ day04/solution_b.py
@@ -0,0 +1,60 @@
+class Card(object):
+    def __init__(self):
+        self.fields = []
+        self.bingo = False
+        for i in range (0, 5):
+            self.fields.append([])
+            for j in range(0, 5):
+                self.fields[i].append(Field())
+
+class Field(object):
+    def __init__(self):
+        self.number = -1
+        self.marked = False
+
+def check_for_bingo(card):
+    for i in range(0, 5):
+        bingo = True
+        for j in range(0, 5):
+            bingo &= card.fields[i][j].marked
+        if bingo:
+            return True
+    for j in range(0, 5):
+        bingo = True
+        for i in range(0, 5):
+            bingo &= card.fields[i][j].marked
+        if bingo:
+            return True
+
+cards = []
+calls = []
+cards = []
+
+with open('day04/input.txt', 'r+') as file:
+    lines = file.readlines()
+    calls = list(map(lambda i: int(i), str.split(lines[0], ",")))
+    c = 2
+    while c < len(lines):
+        card = Card()
+        for i in range(0, 5):
+            for j in range(0, 5):
+                card.fields[i][j].number = int(lines[c+i][0+j*3:3+j*3])
+        cards.append(card)
+        c += 6
+
+for call in calls:
+    for card in cards:
+        for row in card.fields:
+            for field in row:
+                if field.number == call:
+                    field.marked = True
+        card.bingo = check_for_bingo(card)
+    not_winners = list(filter(lambda c: not c.bingo, cards))
+    if len(not_winners) == 1:
+        s = 0
+        for row in not_winners[0].fields:
+            for field in row:
+                if not field.marked:
+                    s += field.number
+        print(s * call)
+        quit()
