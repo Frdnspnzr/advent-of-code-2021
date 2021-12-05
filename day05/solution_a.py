990c8fdab29afcb4be6945af44b6c5addb6b3d73
Pascal Simon <pascaltobiassimon@gmail.com>
2021-12-05 22:01:45 +0100

    Add solution for day 5, part 1

---
+++ day05/solution_a.py
@@ -0,0 +1,41 @@
+from collections import namedtuple
+
+Coordinates = namedtuple("Coordinates", ["x", "y"])
+Line = namedtuple("Line", ["start", "end"])
+
+lines = []
+
+highest_x = 0
+highest_y = 0
+
+with open("input.txt", "r+") as f:
+    for l in f.readlines():
+        c = l.split(" -> ")
+        start = c[0].split(",")
+        end = c[1].split(",")
+        if int(start[0]) == int(end[0]) or int(start[1]) == int(end[1]):
+            lines.append(Line(Coordinates(int(start[0]), int(
+                start[1])), Coordinates(int(end[0]), int(end[1]))))
+            highest_x = max(highest_x, int(start[0]), int(end[0]))
+            highest_y = max(highest_y, int(start[1]), int(end[1]))
+
+count = []
+for x in range(highest_y+1):
+    count.append([])
+    for y in range(highest_x+1):
+        count[x].append(0)
+
+
+for line in lines:
+    for x in range(line.start.x, line.end.x+1):
+        for y in range(line.start.y, line.end.y+1):
+            count[y][x] += 1
+
+dangerous = 0
+
+for y in range(highest_y + 1):
+    for x in range(highest_x + 1):
+        if count[y][x] > 1:
+            dangerous += 1
+
+print(dangerous)
